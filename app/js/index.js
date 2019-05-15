da8e2b3d6f3ecd69c9698c1eeb64d5cccd5f7a7c
albru <bryukhanovspb@gmail.com>
2019-05-15 23:35:54 +0300

    header section

--- app/js/index.js
+++ app/js/index.js
@@ -1,10 +1,10 @@
-import btnFadeInAndSpinner from './btnFadeSpinner'
-import slider from './slider'
-import tabs from './tabs'
-import priceBtnAndPopupHandlers from './priceBtnAndPopupHandlers'
-import scroll from './scroll'
-import sendMail from './sendMail'
-import validation from './validation'
+// import btnFadeInAndSpinner from './btnFadeSpinner'
+// import slider from './slider'
+// import tabs from './tabs'
+// import priceBtnAndPopupHandlers from './priceBtnAndPopupHandlers'
+// import scroll from './scroll'
+// import sendMail from './sendMail'
+// import validation from './validation'
 
 
 
